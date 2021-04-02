db461ded03f93d0b33a4234f6023ec16fd743854
BradleyWilliamsDev <79307883+BradleyWilliamsDev@users.noreply.github.com>
2021-04-02 16:06:40 +0200

    Create Deck and Game Manager to handle card Values

---
+++ 200138_BradleyWilliams_GX201_BlackJack/Assets/Scripts/DeckManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+
+    public Sprite[] cardsToBeDealt;
+    private int[] cardValues = new int[53];
+    int deckStartingNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void ValueOfCard()
+    {
+        int num = 0;
+        //This loop goes through the deck and assigns each value to the cards
+        for (int i = 0; i < cardsToBeDealt.Length; i++)
+        {
+            //This makes sure the program goes through every card
+            num = i;
+            //Mod 13 takes the remainder of the card position and that is the cards Value.
+            num %= 13;
+            if (num > 10 || num == 0)
+            {
+                num = 10;
+            }
+            cardValues[i] = num++;
+        }
+        deckStartingNum = 1;
+    }
+}
---
+++ 200138_BradleyWilliams_GX201_BlackJack/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //Make buttons public so other scripts can acess them
+    public Button dealButton, hitButton, stayButton, betButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
