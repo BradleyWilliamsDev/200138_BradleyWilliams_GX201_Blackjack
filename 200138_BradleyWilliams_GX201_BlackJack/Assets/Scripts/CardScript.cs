3546141452dcb313cf813f2f490895a7b42c0d58
BradleyWilliamsDev <79307883+BradleyWilliamsDev@users.noreply.github.com>
2021-04-02 16:43:45 +0200

    Set Up Card systems

---
+++ 200138_BradleyWilliams_GX201_BlackJack/Assets/Scripts/CardScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    //This is a class that acts as a struct to store the data of the cards
+
+    public int cardValue = 0;
+
+    public int GetValueOfCard()
+    {
+        return cardValue;
+    }
+
+    public void SetCardValue(int newValue)
+    {
+        cardValue = newValue;
+    }
+
+    public string GetCardName()
+    {
+        return GetComponent<SpriteRenderer>().sprite.name;
+    }
+
+    public void SetCardSprite(Sprite newCardSprite)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = newCardSprite;
+    }
+
+    //When the player wants to play again, or if the player whats to restart
+    //we need to reset the card and place it in the deck
+    public void ResetCard()
+    {
+        Sprite back = GameObject.Find("CardDeck").GetComponent<DeckManager>().GetCardBack();
+        gameObject.GetComponent<SpriteRenderer>().sprite = back;
+        cardValue = 0;
+    }
+}
--- 200138_BradleyWilliams_GX201_BlackJack/Assets/Scripts/DeckManager.cs
+++ 200138_BradleyWilliams_GX201_BlackJack/Assets/Scripts/DeckManager.cs
@@ -11,7 +11,7 @@ public class DeckManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,5 +32,39 @@ public class DeckManager : MonoBehaviour
             cardValues[i] = num++;
         }
         deckStartingNum = 1;
+    }
+
+    //Make function public so that other classes can access it
+    public void Shuffle()
+    {
+        //The loop goes backwards through the deck swapping cards in the array around -1 to get the correct number of elements to exclude the back of the card
+        for (int i = cardsToBeDealt.Length - 1; i > 0; i--)
+        {
+            //FloorToInt takes away the decimal in a float and makes it an int, gets a random number in the deck to be the value of j, 
+            //then swaps i and j around and finally sets the value and card face of J
+            int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * cardValues.Length - 1) + 1;
+            Sprite cardFace = cardsToBeDealt[i];
+            cardsToBeDealt[i] = cardsToBeDealt[j];
+            cardsToBeDealt[j] = cardFace;
+
+            int cardValue = cardValues[i];
+            cardValues[i] = cardValues[j];
+            cardValues[j] = cardValue;
+        }
+    }
+
+    // pulblic for other classes to access
+    public int DealCard(CardScript cardScript)
+    {
+        //Use card script to deal and set the card sprite
+        cardScript.SetCardSprite(cardsToBeDealt[deckStartingNum]);
+        //Gets the card value then moves to the next position in the deck
+        cardScript.SetCardValue(cardValues[deckStartingNum++]);
+        return cardScript.GetValueOfCard();
+    }
+
+    public Sprite GetCardBack()
+    {
+        return cardsToBeDealt[0];
     }
 }
